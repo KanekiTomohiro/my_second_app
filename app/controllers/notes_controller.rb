35d6d98fa1ad04d6be2c472b87d7a1294b97dcfa
yuta_mizogami <mizogami@temona.co.jp>
2017-04-25 15:51:19 +0900

    編集機能、削除機能

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -14,16 +14,34 @@ class NotesController < ApplicationController
   	@note.save
 
   	#showアクションへのリダイレクト
-  	redirect_to note_path(@note.id)
+  	redirect_to notes_path
 
   end
 
   def index
-  	@notes = Note.all
+  	@notes = Note.all.order(created_at: :desc)
   end
 
   def show
   	@note = Note.find(params[:id])
+  end
+
+  def edit
+  	@note = Note.find(params[:id])
+  end
+
+  def update
+  	@note = Note.find(params[:id])
+  	@note.title = params[:title]
+  	@note.content = params[:content]
+  	@note.save
+  	redirect_to note_path(@note.id)
+  end
+
+  def destroy
+  	@note = Note.find(params[:id])
+  	@note.destroy
+  	redirect_to notes_path
   end
 
 end
